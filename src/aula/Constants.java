cce8cd385e33ca30d11c5f84ace91ff10f8ebe63
Streppel <streppels@gmail.com>
2015-05-11 23:52:59 -0300

    removed clutter, got inserts queryMap working

--- src/aula/Constants.java
+++ src/aula/Constants.java
@@ -1,15 +1,32 @@
 package aula;
 
+/**
+ * This class holds the constants used throughout the code
+ * @author natan
+ *
+ */
 public class Constants {
 	public final static String INVALID_INPUT = "INVALID_INPUT";
 	
-	public final static String INSERT = "INSERT";
+	public final static String ACAO = "acao";
 	
-	public final static String DELETE = "DELETE";
+	public final static String INSERT = "insert";
 	
-	public final static String UPDATE = "UPDATE";
+	public final static String DELETE = "delete";
 	
-	public final static String QUERY = "QUERY";
+	public final static String UPDATE = "update";
 	
-	public final static String HELP = "HELP";
+	public final static String QUERY = "query";
+	
+	public final static String HELP = "help";
+	
+	public final static String VALOR = "valor";
+	
+	public final static String ANO = "valor";
+	
+	public final static String QTD = "qtd";
+	
+	public final static String NOME_VINHO = "nome";
+	
+	public final static String ORIGEM = "origem";
 }
--- src/aula/Crud.java
+++ src/aula/Crud.java
@@ -3,6 +3,9 @@ package aula;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.regex.Matcher;
 
 import com.mongodb.BasicDBObject;
@@ -13,46 +16,22 @@ import com.mongodb.MongoClient;
 
 /**
  *
- * @author 123244
  */
+@SuppressWarnings("unused")
 public class Crud {
     
 	private boolean running = true;
 	
-	/**
-	 * Vari�vel usada para validar as entradas do usu�rio
-	 */
 	private RegexValidator regexValidator = new RegexValidator();
 	
     public Crud() {
-
+    	/*
+    	 * TODO initializate JDBC
+    	 */
     }
     
-    /* regex:
-     * 	String line = "insert {vinho: Intenso maria pode, ano: 1978, origem: Londres, valor: 5600.64, qtd: 4}";
-	    String pattern = "(^[a-zA-Z]+) \\{(\\w+): (.+), (\\w+): (.+), (\\w+): (.+), (\\w+): (.+)}";
-	    Pattern r = Pattern.compile(pattern);
-	    
-	    Matcher m = r.matcher(line);
-	    
-	    if (m.find( )) {
-	         System.out.println("Found value: " + m.group(0) );
-	         System.out.println("Found value: " + m.group(1) );
-	         System.out.println("Found value: " + m.group(2) );
-	         System.out.println("Found value: " + m.group(3) );
-	         System.out.println("Found value: " + m.group(4) );
-	         System.out.println("Found value: " + m.group(5) );
-	         System.out.println("Found value: " + m.group(6) );
-	         System.out.println("Found value: " + m.group(7) );
-	         System.out.println("Found value: " + m.group(8) );
-	         System.out.println("Found value: " + m.group(9) );
-	      } else {
-	         System.out.println("NO MATCH");
-	    }
-     */
-    
     public void run() {
-        // reads output from console
+        // reads input from console
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         
         while (running) {
@@ -60,10 +39,12 @@ public class Crud {
         		System.out.println(Messages.WAIT_INPUT);
                 String userInput = reader.readLine();
                 
-                ArrayList<String> data = parseInput(userInput);
-                for (String e: data) {
-                	System.out.println(e);
+                HashMap<String, String> queryMap = parseInput(userInput);
+                
+                for (Map.Entry<String, String> entry : queryMap.entrySet()) {
+                	System.out.println(entry.getKey() + " : " + entry.getValue());
                 }
+                
                 
         	} catch (Exception e) {
         		handlesExcp(e);
@@ -71,32 +52,49 @@ public class Crud {
         } 
     }
     
-    private void handlesExcp(Exception e) {
-    	 if (e.getMessage().equals(Constants.INVALID_INPUT)) {
-			 System.out.println(Messages.INVALID_INPUT_WARN);
-         }
-    }
-    
-    private ArrayList<String> parseInput(String userInput) throws Exception {
-    	ArrayList<String> mArray = new ArrayList<String>();
+    private HashMap<String, String> parseInput(String userInput) throws Exception {
+    	HashMap<String, String> queryMap = new HashMap<>();
     	
     	String action = getAction(userInput);
     	
-		switch (action.toUpperCase()) {
+    	Matcher m = null;
+		switch (action.toLowerCase().trim()) {
     		case Constants.INSERT:
-    			Matcher m = regexValidator.validateInsert(userInput);
+    			m = regexValidator.validateInsert(userInput);
+    			if (m.find()) {
+    				queryMap.put(Constants.ACAO, Constants.INSERT);
+    				queryMap.put(Constants.NOME_VINHO, m.group(3));
+    				queryMap.put(Constants.ANO, m.group(5));
+    				queryMap.put(Constants.ORIGEM, m.group(7));
+    				queryMap.put(Constants.VALOR, m.group(9));
+    				queryMap.put(Constants.QTD, m.group(11));
+    			} else {
+    				throw new Exception(Constants.INVALID_INPUT);
+    			}
+    			break;
+    		case Constants.DELETE:
+    			m = regexValidator.validateDelete(userInput);
     			if (m.find()) {
     				
     			} else {
     				throw new Exception(Constants.INVALID_INPUT);
     			}
-//	    			insert
-    			break;
-    		case Constants.DELETE:
     			break;
     		case Constants.UPDATE:
+    			m = regexValidator.validateUpdate(userInput);
+    			if (m.find()) {
+    				
+    			} else {
+    				throw new Exception(Constants.INVALID_INPUT);
+    			}
     			break;
     		case Constants.QUERY:
+    			m = regexValidator.validateQuery(userInput);
+    			if (m.find()) {
+    				
+    			} else {
+    				throw new Exception(Constants.INVALID_INPUT);
+    			}
     			break;
     		case Constants.HELP:
     			System.out.println(Messages.HELP);
@@ -108,7 +106,7 @@ public class Crud {
     		default:
     			throw new Exception(Constants.INVALID_INPUT);
     	}
-    	return mArray;
+    	return queryMap;
     }
     
     /**
@@ -117,6 +115,12 @@ public class Crud {
     private String getAction(String userInput) {
     	return userInput.split("\\{")[0];
     	
+    }
+    
+    private void handlesExcp(Exception e) {
+    	if (e.getMessage().equals(Constants.INVALID_INPUT)) {
+    		System.out.println(Messages.INVALID_INPUT_WARN);
+    	}
     }
     
     public void greeting() {
