d6385f0fce79714c3bdad6398bd63b71fa04ab91
Streppel <streppels@gmail.com>
2015-05-11 23:50:03 -0300

    updated regex interfaces

--- src/aula/RegexFactory.java
+++ src/aula/RegexFactory.java
@@ -2,42 +2,51 @@ package aula;
 
 import java.util.regex.Pattern;
 
+/**
+ * Creates the regexes used through predefined business rules of input
+ *
+ */
 public class RegexFactory {
 	
 	/**
 	 * INSERT_PATTERN
 	 * insert working example: insert {vinho: Chateau Ste, ano: 1997, origem: France, valor: 500.00, qtd: 3}
+	 * 
+	 * This is pretty huge indeed. No time to make it pretty though XD
 	 */
 	private static final String INSERT_PATTERN = "(^[a-zA-Z]+)\\s*\\{(\\w+):\\s*([a-zA-Z\\s]+),\\s*(\\w+):\\s*([0-9\\s]+),\\s*(\\w+):\\s*([a-zA-Z\\s]+),\\s*(\\w+):\\s*([0-9.(0-9)\\s]+),\\s*(\\w+):\\s*([0-9]+)\\s*}";
 	
 	/**
 	 * DELETE_PATTERN
-	 * 
+	 * TODO
 	 */
+	private static final String DELETE_PATTERN = "";
 	
 	/**
 	 * UPDATE_PATTERN 
-	 * 
+	 * TODO
 	 */
+	private static final String UPDATE_PATTERN = "";
 	
 	/**
 	 *  QUERY_PATTERN
-	 *  
+	 *  TODO
 	 */
+	private static final String QUERY_PATTERN = "";
 	
 	static Pattern getInsertPattern() {
 		return Pattern.compile(INSERT_PATTERN);
 	}
 	
 	static Pattern getDeletePattern() {
-		return Pattern.compile(INSERT_PATTERN);
+		return Pattern.compile(DELETE_PATTERN);
 	}
 	
 	static Pattern getUpdatePattern() {
-		return Pattern.compile(INSERT_PATTERN);
+		return Pattern.compile(UPDATE_PATTERN);
 	}
 	
 	static Pattern getQueryattern() {
-		return Pattern.compile(INSERT_PATTERN);
+		return Pattern.compile(QUERY_PATTERN);
 	}
 }
--- src/aula/RegexValidator.java
+++ src/aula/RegexValidator.java
@@ -3,17 +3,34 @@ package aula;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * This class validates common inputs of this program through predefined regexes 
+ * made in the RegexFactory class
+ *
+ */
 public class RegexValidator {
 
 	private final Pattern INSERT_PATTERN = RegexFactory.getInsertPattern();
 	
-//	private final Pattern DELETE_PATTERN = RegexFactory.getDeletePattern();
+	private final Pattern DELETE_PATTERN = RegexFactory.getDeletePattern();
 	
-//	private final Pattern UPDATE_PATTERN = RegexFactory.getDeletePattern();
+	private final Pattern UPDATE_PATTERN = RegexFactory.getDeletePattern();
 	
-//	private final Pattern QUERY_PATTERN = RegexFactory.getDeletePattern();
+	private final Pattern QUERY_PATTERN = RegexFactory.getDeletePattern();
 	
 	public Matcher validateInsert(String input) {
 		return INSERT_PATTERN.matcher(input);
+	}
+	
+	public Matcher validateDelete(String input) {
+		return DELETE_PATTERN.matcher(input);
+	}
+	
+	public Matcher validateUpdate(String input) {
+		return UPDATE_PATTERN.matcher(input);
+	}
+	
+	public Matcher validateQuery(String input) {
+		return QUERY_PATTERN.matcher(input);
 	}
 }
